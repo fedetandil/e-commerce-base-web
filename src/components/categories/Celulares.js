ceb3779a527eeea91b7a3dc7780d18b7ad3fd184
Federico Aceto <federicoagustinaceto@gmail.com>
2022-08-14 20:12:08 -0300

    Multiple products on categories

--- src/components/categories/Celulares.js
+++ src/components/categories/Celulares.js
@@ -1,11 +1,51 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Celulares() {
-    return <>
+    let cellphones = [
+        {
+            "name" : "Celular Samsung S20",
+            "description" :"Descripcion del celular",
+            "image" :'SamsungS20'
+        },
+        {
+            "name" : "Celular Samsung S20",
+            "description" :"Descripcion del celular",
+            "image" :'SamsungS20'
+        },
+        {
+            "name" : "Celular Samsung S20",
+            "description" :"Descripcion del celular",
+            "image" :'SamsungS20'
+        }
+    ]
+    let mypage = [];
+    let keyCount = 1;
+    function loadProducts(){
+        cellphones.map((cell,index) => {
+            mypage.push( <Card key={keyCount} style={{ width: '18rem' }}>
+                <Card.Img variant="top"  src={`../../images/categories/celulares/${cell.image}.png`}
+                 />
+                <Card.Body>
+                    <Card.Title>{cell.name}</Card.Title>
+                    <Card.Text>
+                        {cell.description}
+                    </Card.Text>
+                    <Button variant="primary">Agregar al carrito</Button>
+                </Card.Body>
+            </Card>);
 
-        <div>Celulares</div>
+            keyCount++;
+        })
+        return mypage;
+    }
 
-    </>;
+    return (
+        loadProducts()
+    );
 
 }
+
+
 
 export default Celulares;
